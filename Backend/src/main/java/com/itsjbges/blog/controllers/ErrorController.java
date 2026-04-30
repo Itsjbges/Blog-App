ed54a0a8cb1d7b786916b47599abd04059f759f0
Itsjbges <jasonbriansetiawanyt@gmail.com>
2026-04-30 20:26:26 +0200

    Add error controller

---
+++ Backend/src/main/java/com/itsjbges/blog/controllers/ErrorController.java
@@ -0,0 +1,42 @@
+package com.itsjbges.blog.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itsjbges.blog.domain.dtos.ApiErrorResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        log.error("Caugt exception", ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        .message("An unexpected error occured")
+        .build();
+
+        return new ResponseEntity<>(
+            error, HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException (IllegalArgumentException ex) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+        .status(HttpStatus.BAD_REQUEST.value())
+        .message(ex.getMessage())
+        .build();
+
+        return new ResponseEntity<>(
+            error, HttpStatus.BAD_REQUEST
+        );
+    }
+}
