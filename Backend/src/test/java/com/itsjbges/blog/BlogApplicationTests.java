2a78719b16cbaa2916ebcf7bee883f91a36a983f
Itsjbges <jasonbriansetiawanyt@gmail.com>
2026-04-30 21:09:45 +0200

    Fix test case

--- Backend/src/test/java/com/itsjbges/blog/BlogApplicationTests.java
+++ Backend/src/test/java/com/itsjbges/blog/BlogApplicationTests.java
@@ -2,9 +2,19 @@ package com.itsjbges.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogApplicationTests {
+
+	static {
+        // This ensures the variables are set before the Spring Context even starts
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
 	@Test
 	void contextLoads() {
