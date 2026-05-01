36516e9e364c972b88b297c7527c9c79d99c9113
Itsjbges <jasonbriansetiawanyt@gmail.com>
2026-05-01 19:57:53 +0200

    Implement authentication endpoint

---
+++ Backend/src/main/java/com/itsjbges/blog/controllers/AuthController.java
@@ -0,0 +1,41 @@
+package com.itsjbges.blog.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itsjbges.blog.domain.dtos.AuthResponse;
+import com.itsjbges.blog.domain.dtos.LoginRequest;
+import com.itsjbges.blog.services.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        UserDetails userDetails = authenticationService.authenticate(
+            loginRequest.getEmail(), 
+            loginRequest.getPassword());
+
+        String tokenValue = authenticationService.generateToken(userDetails);
+
+        AuthResponse authResponse = AuthResponse.builder()
+        .token(tokenValue)
+        .expiresIn(86400)
+        .build();
+
+        return ResponseEntity.ok(authResponse);
+
+    }
+
+}
